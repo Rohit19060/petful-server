b709ec4c026d38ea6e9c763d3e3824bef04b3f2e
Rohit19060 <rohitjain19060@gmail.com>
2021-01-21 19:00:59 +0530

    Vercel Ready

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./modules/app/app");
-const { PORT } = require("module/app/config");
+const { PORT } = require("./modules/app/config");
 
 app.listen(PORT, () => {
   console.log(`[petful-server] Listening on ${PORT}.`);
